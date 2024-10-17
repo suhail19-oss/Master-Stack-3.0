40455b836789f35655394ae73c58c004f1b15227
AKHTAR HUSSAIN <hussain2003akhtar@gmail.com>
2024-10-17 19:37:26 +0530

    added module and created server and database for backend

---
+++ backend/src/server.js
@@ -0,0 +1,19 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import { connectDB } from "./config/db.js";
+
+dotenv.config();
+const app = express();
+app.use(cors());
+connectDB();     
+app.use(express.json());
+
+app.get("/test", (req, res) => {
+  res.send("Helo from server");
+});
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log(`Server is running on PORT : ${PORT}`);
+});
